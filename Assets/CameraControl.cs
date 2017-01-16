57b3a06d0611f630b7c8bed4de17587abc1464e8
THE_BA_MACHINE\Ben <bcalbert777@gmail.com>
2017-01-16 11:47:46 -0500

    Fixed Camera Look and Rotation around Cube

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class CameraControl : MonoBehaviour {
+
+        PlayerController PlayerController;
+
+        // Use this for initialization
+        void Start()
+        {
+            PlayerController = FindObjectOfType<PlayerController>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+           // transform.position = PlayerController.transform.position;
+        }
+}
+}
--- Assets/Player/MouseLook.cs
+++ Assets/Player/MouseLook.cs
@@ -25,14 +25,21 @@ namespace Player
         private float initZpos;
         private float zPos; 
         private float initYpos;
-        private float yPos; 
+        private float yPos;
+        private float initXpos;
+        private float xPos;
+        private float r;
+        private float initR;
+        private Quaternion CameraAngles;
 
         public void Init(Transform character, Transform camera)
         {
             m_CharacterTargetRot = character.localRotation;
             m_CameraTargetRot = camera.localRotation;
-            initZpos = camera.localPosition.z;
-            initYpos = camera.localPosition.y; 
+            zPos = camera.localPosition.z;
+            initYpos = camera.localPosition.y;
+            xPos = camera.localPosition.x;
+            initR = Mathf.Sqrt(Mathf.Pow(zPos, 2) + Mathf.Pow(xPos, 2));
             //Debug.Log(initZpos);
         }
 
@@ -41,47 +48,52 @@ namespace Player
         {
             float yRot = CrossPlatformInputManager.GetAxis("Mouse X") * XSensitivity;
             float xRot = CrossPlatformInputManager.GetAxis("Mouse Y") * YSensitivity;
-
-            m_CharacterTargetRot *= Quaternion.Euler (0f, yRot, 0f);
-            m_CameraTargetRot *= Quaternion.Euler (-xRot, 0f, 0f);
-
-            float adjustmentFactorZ = .1f;
-            float adjustmentFactorY = .17f;
-            if (180 - camera.rotation.eulerAngles.x < 0)
-            {
-                zPos = ((380 - camera.rotation.eulerAngles.x)) * adjustmentFactorZ + initZpos;
-                yPos = ((camera.rotation.eulerAngles.x - 380)) * adjustmentFactorY + initYpos;
-            }
-            else if (camera.rotation.eulerAngles.x <= 20)
-            {
-                zPos = (20 - (camera.rotation.eulerAngles.x)) * adjustmentFactorZ + initZpos;
-                yPos = ((camera.rotation.eulerAngles.x - 20)) * adjustmentFactorY + initYpos; ;
-            }
-            else
-            {
-                zPos = (camera.rotation.eulerAngles.x - 20) * adjustmentFactorZ + initZpos;
-                yPos = initYpos;
-            }
-
-            camera.localPosition = new Vector3(camera.localPosition.x, yPos, zPos);
+            //Debug.Log(yRot);
+          //  m_CharacterTargetRot *= Quaternion.Euler (0f, yRot, 0f);
+            //m_CameraTargetRot *= Quaternion.Euler (-xRot, 0f, 0f);
+            m_CameraTargetRot *= Quaternion.Euler(-xRot, yRot, 0);
 
 
-            if (clampVerticalRotation)
-                m_CameraTargetRot = ClampRotationAroundXAxis (m_CameraTargetRot);
-
+            //float initR = Mathf.Sqrt(Mathf.Pow(zPos, 2) + Mathf.Pow(xPos, 2));
+ 
+           
             if(smooth)
             {
-                character.localRotation = Quaternion.Slerp (character.localRotation, m_CharacterTargetRot,
-                    smoothTime * Time.deltaTime);
-                camera.localRotation = Quaternion.Slerp (camera.localRotation, m_CameraTargetRot,
-                    smoothTime * Time.deltaTime);
+                // character.localRotation = Quaternion.Slerp (character.localRotation, m_CharacterTargetRot,
+                //    smoothTime * Time.deltaTime);
+                float xAngle;
+                if (m_CameraTargetRot.eulerAngles.x > 180)
+                {
+                    xAngle = -(360 - m_CameraTargetRot.eulerAngles.x);
+                }
+                else
+                {
+                    xAngle = m_CameraTargetRot.eulerAngles.x;
+                }
+
+                CameraAngles = Quaternion.Euler(Mathf.Clamp(xAngle, MinimumX, MaximumX), m_CameraTargetRot.eulerAngles.y, 0);
+                
+                camera.localRotation = Quaternion.Slerp (camera.localRotation, CameraAngles,
+                   smoothTime * Time.deltaTime);
             }
             else
             {
-                character.localRotation = m_CharacterTargetRot;
-                camera.localRotation = m_CameraTargetRot;
+                float xAngle; 
+                if (m_CameraTargetRot.eulerAngles.x > 180)
+                {
+                    xAngle = -(360 - m_CameraTargetRot.eulerAngles.x);
+                }
+                else
+                {
+                    xAngle = m_CameraTargetRot.eulerAngles.x;
+                }
+                //  float xAngle = Mathf.Clamp(m_CameraTargetRot.eulerAngles.x + 90, MinimumX + 90, MaximumX + 90) - 90f;
+                CameraAngles = Quaternion.Euler(Mathf.Clamp(xAngle, MinimumX, MaximumX), m_CameraTargetRot.eulerAngles.y, 0);
+                camera.localRotation = CameraAngles;
+
             }
 
+            CreateCameraPosition(camera);
             UpdateCursorLock();
         }
 
@@ -125,21 +137,34 @@ namespace Player
             }
         }
 
-        Quaternion ClampRotationAroundXAxis(Quaternion q)
+        void CreateCameraPosition(Transform camera)
         {
-            q.x /= q.w;
-            q.y /= q.w;
-            q.z /= q.w;
-            q.w = 1.0f;
+            float adjustmentFactorR = .1f;
+            float adjustmentFactorY = .17f;
+            if (180 - CameraAngles.eulerAngles.x < 0)
+            {
+                r = ((380 - CameraAngles.eulerAngles.x)) * adjustmentFactorR + initR;
+                yPos = ((CameraAngles.eulerAngles.x - 380)) * adjustmentFactorY + initYpos;
 
-            float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan (q.x);
+            }
+            else if (CameraAngles.eulerAngles.x <= 20)
+            {
+                r = ( (CameraAngles.eulerAngles.x) - 20) * adjustmentFactorR + initR;
+                yPos = ((CameraAngles.eulerAngles.x - 20)) * adjustmentFactorY + initYpos; ;
+            }
+            else
+            {
+                r = (20 - CameraAngles.eulerAngles.x) * adjustmentFactorR + initR;
+                yPos = initYpos;
+            }
+            Debug.Log(CameraAngles.eulerAngles.x);
+            //pythagrion theorm
 
-            angleX = Mathf.Clamp (angleX, MinimumX, MaximumX);
+            xPos = r * Mathf.Sin(Mathf.Deg2Rad * -m_CameraTargetRot.eulerAngles.y);
+            zPos = -r * Mathf.Cos(Mathf.Deg2Rad * -m_CameraTargetRot.eulerAngles.y);
 
-            q.x = Mathf.Tan (0.5f * Mathf.Deg2Rad * angleX);
-
-            return q;
+            //camera.localPosition = new Vector3(camera.localPosition.x, yPos, zPos);
+            camera.localPosition = new Vector3(xPos, yPos, zPos);
         }
-
     }
 }
